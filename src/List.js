45ed9d0f4254f1aa9917b48d0d9a6e031f27d87e
zhzlatkov <zhzlatkov@gmail.com>
2022-08-25 11:24:17 +0300

    done with grocery list

--- src/List.js
+++ src/List.js
@@ -1,7 +1,34 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className="gorcery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button
+                className="edit-btn"
+                type="button"
+                onClick={() => editItem(id)}
+              >
+                <FaEdit></FaEdit>
+              </button>
+              <button
+                className="delete-btn"
+                type="button"
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash></FaTrash>
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
